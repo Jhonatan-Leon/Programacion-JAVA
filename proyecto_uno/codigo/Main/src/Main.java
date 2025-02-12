4602968e1034e87daea15ce1187d250d6a48f27a
jonathan cardona <c.jhonatan2007@gmail.com>
2025-02-11 21:00:12 -0500

    Primer repo y agregue proyecto base

---
+++ proyecto_uno/codigo/Main/src/Main.java
@@ -0,0 +1,41 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        list_inversa();
+    }
+
+    public static void list_inversa(){
+        /*
+        Escriba un programa que pida al usuario ingresar 5 nombres y los almacene en una lista.
+        Luego, muestre los nombres en orden inverso al que fueron ingresados.
+        Abstrabccion
+        ¿Que se solicita finalmente?
+        Imprimir los nombres ingresados de manera inversa
+        ¿Que información es relevante?
+        - Ingresar 5 nomrbes
+        - Almancenar en una lista
+        - cambiar el orden de los nombres
+        Descomposición
+        - Ingresar nombres
+        - Lista_nombres
+        - invertir la lista de los nombres
+        - generar mensaje
+        - imprimir mensaje
+
+         */
+        String[] carlos = {"Juan","Carlos","Felipe","Daniel","Jose"};
+
+        final max = 5;
+        String[] list_inverse = {};
+        while(max > carlos.length){
+            list_inverse.pu
+        }
+
+
+
+
+
+    }
+}
+
